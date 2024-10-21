065875179922d4f8ec35d6be5acf0362fe6b9837
Douglas Alves <99148968+douglasalves01@users.noreply.github.com>
2024-10-20 21:25:00 -0300

    Add files via upload

---
+++ src/main/java/tech/Avalie/AvalieApplication.java
@@ -0,0 +1,13 @@
+package tech.Avalie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvalieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvalieApplication.class, args);
+	}
+
+}
